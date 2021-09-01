34691d76510a7c703e2a6afbd220924c514884d1
aanacif <aanacif@gmail.com>
2021-09-01 10:19:44 -0300

    updt

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,37 @@
-console.log('HELLO');
+let calc = document.querySelector('.calc-container')
+let tipAmountPerson = document.querySelector('.calc-tip-amount h1').textContent
+let tipAmountTotal = document.querySelector('.calc-total h1').textContent
+//Bill
+let calcBill = document.querySelector('.calc-bill')
+//Number of people
+let calcPeople = document.querySelector('.calc-people')
+//reset button
+let calcReset = document.querySelector('.reset')
 
-const test = () => {
-	console.log('this is a test');
-};
+
+
+calc.addEventListener('keyup', () => {
+	let totalPerPerson = calcTotalPerPerson(calcBill.value, calcPeople.value)
+	if (totalPerPerson) {
+		document.querySelector('.calc-total h1').textContent = `$${totalPerPerson}`
+	}
+})
+
+
+
+function round(num) {
+	let m = Number((Math.abs(num) * 100).toPrecision(15));
+	return Math.round(m) / 100 * Math.sign(num);
+}
+
+calcReset.addEventListener('click', () => {
+	document.querySelector('.calc-tip-amount h1').textContent = '$0.00'
+	document.querySelector('.calc-total h1').textContent = '$0.00'
+})
+
+function calcTotalPerPerson(bill, people) {
+	let result = bill / people
+	if (Number.isFinite(result) && !isNaN(result)) {
+		return round(result)
+	}
+}
