672b1725f08fd01b2caf6bf4e8742513169ef11b
aanacif <aanacif@gmail.com>
2021-09-03 19:23:41 -0300

    shipIt

--- app/js/script.js
+++ app/js/script.js
@@ -16,13 +16,13 @@ percentageSelectEvent.addEventListener('change', (event) => {
 	for (let counter = 0; counter < child.length; counter++) {
 		if (child[counter].classList.contains('active')) {
 			child[counter].classList.remove('active')
+			customInput.value = ''
 		}
 	}
 	let parent = event.target?.offsetParent
 	parent.classList.toggle('active')
 	checkOut()
 })
-
 //zero-people-alert
 let zeroAlert = document.querySelector('.zero-people')
 calcPeople.addEventListener('keyup', () => {
@@ -32,16 +32,10 @@ calcPeople.addEventListener('keyup', () => {
 		zeroAlert.style.opacity = 0
 	}
 })
-
-function round(num) {
-	let m = Number((Math.abs(num) * 100).toPrecision(15));
-	return Math.round(m) / 100 * Math.sign(num);
-}
-
+const customInput = document.querySelector('.calc-percs input[name="option-custom-input"')
 const checkForActives = document.querySelector('.calc-percs')
 const clearActives = () => {
 	let child = checkForActives.children
-
 	for (let counter = 0; counter < child.length; counter++) {
 		if (child[counter].classList.contains('active')) {
 			child[counter].classList.remove('active')
@@ -53,15 +47,15 @@ calcReset.addEventListener('click', () => {
 	document.querySelector('.calc-total h1').textContent = '$0.00'
 	clearActives()
 })
-
 function calcPercentage(bill, percentage) {
-	if (bill && percentage) {
+	if (percentage == 0) return 0
+	if (bill > 0 && percentage > 0) {
 		let result = bill * (percentage / 100)
 		return round(result)
 	}
 }
 function calcTotalPerPerson(bill, people) {
-	if (bill && people) {
+	if (bill > 0 && people > 0) {
 		let result = bill / people
 		if (Number.isFinite(result) && !isNaN(result)) {
 			return round(result)
@@ -69,28 +63,56 @@ function calcTotalPerPerson(bill, people) {
 	}
 	return
 }
-
-calc.addEventListener('keyup', () => {
-	checkOut()
-})
-
-const checkOut = () => {
-	const totalPerPerson = calcTotalPerPerson(calcBill.value, calcPeople.value)
-	let totalPercentage = calcPercentage(calcBill.value, selectedPercentage())
-	if (totalPerPerson > 0) {
-		document.querySelector('.calc-total h1').textContent = `$${round(totalPerPerson + round((totalPercentage / calcPeople.value)))}`
+// EVENT LISTENERS
+// customInput.addEventListener('keyup', (event) => {
+// 	checkOut(event.target.value)
+// })
+calc.addEventListener('keyup', (event) => {
+	if (calcBill.value > 0 && calcPeople.value > 0) {
+		if (event.target.name == 'option-custom-input') {
+			checkOut(event.target.value)
+		} else {
+			checkOut()
+		}
 	}
-	if (totalPercentage > 0 && calcPeople.value) {
-		totalPercentage = totalPercentage / calcPeople.value
+})
+const checkOut = (custom) => {
+	if (custom) {
+		let totalPerPerson = calcTotalPerPerson(calcBill.value, calcPeople.value)
+		let totalPercentage = calcPercentage(calcBill.value, round(custom))
+		document.querySelector('.calc-total h1').textContent = `$${round(totalPerPerson + (totalPercentage / calcPeople.value))}`
 		document.querySelector('.calc-tip-amount h1').textContent = `$${round(totalPercentage)}`
+		return
+	}
 
+	let totalPerPerson = calcTotalPerPerson(calcBill.value, calcPeople.value)
+	let totalPercentage = calcPercentage(calcBill.value, selectedPercentage())
+
+	if (totalPerPerson > 0) {
+		document.querySelector('.calc-total h1').textContent = `$${round(totalPerPerson + (totalPercentage / calcPeople.value))}`
+	}
+	if (totalPercentage == 0) {
+		document.querySelector('.calc-tip-amount h1').textContent = `$0.00`
+	} else {
+		let totalPercentage = calcPercentage(calcBill.value, selectedPercentage())
+		document.querySelector('.calc-tip-amount h1').textContent = `$${round(totalPercentage)}`
 	}
 }
-
 const selectedPercentage = () => {
 	let isPercentageSelected = document.querySelectorAll('.calc-percs input[name="options"]:checked');
-	if (isPercentageSelected) {
-		return round(isPercentageSelected[0]?.value)
+	let isCustomSelected = customInput.value
+
+	if (isPercentageSelected.length > 0) {
+		return isPercentageSelected[0]?.value
 	}
-	return 1
+	if (isCustomSelected > 0) {
+		return isCustomSelected
+	} else {
+		return 0
+	}
+}
+//rounding numbers function
+function round(num) {
+	let m = Number((Math.abs(num) * 100).toPrecision(15));
+	return Math.round(m) / 100 * Math.sign(num);
 }
